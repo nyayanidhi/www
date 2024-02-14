415ed5c7df829ab0fd0f5fdf3b42dc906ed9e41c
Adithya Krishna <aadithya794@gmail.com>
2024-02-14 23:58:15 +0530

    chore: removed video
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- components/hero.tsx
+++ components/hero.tsx
@@ -41,14 +41,14 @@ export default function Hero() {
           </div>
 
           {/* Hero image */}
-          <ModalVideo
+          {/* <ModalVideo
             thumb={VideoThumb}
             thumbWidth={768}
             thumbHeight={432}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
-            videoHeight={1080} />
+            videoHeight={1080} /> */}
 
         </div>
 
